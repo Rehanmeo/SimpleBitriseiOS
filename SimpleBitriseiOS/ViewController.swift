35bc098abd547909d27d791d8501636b781e8fbb
Muhammad Rehan <mrehanmeo@gmail.com>
2022-08-28 17:35:10 +0500

    Initial Commit

---
+++ SimpleBitriseiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleBitriseiOS
+//
+//  Created by Muhammad Rehan on 8/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
